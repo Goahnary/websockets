96d10be8f15715c0215cb0525101c5b6a57f9436
Noah Gary <goahnary@gmail.com>
2016-02-12 14:20:59 -0600

    server now exits when it gets the message 'exit'

--- socket-server.py
+++ socket-server.py
@@ -5,11 +5,12 @@ import socket
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5822
 BUFFER_SIZE = 128
+data = ""
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', TCP_PORT))
 
-while True:
+while data != "EXIT":
 	s.listen(1)
 	conn, addr = s.accept()
 	print 'connection address: ', addr

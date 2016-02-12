26e9ac3bd6810fa62dedeb118cf8d224726725a6
Noah Gary <goahnary@gmail.com>
2016-02-12 20:16:06 +0000

    Modified client script to send inputs multiple times

---
+++ client-connect.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import socket
+
+TCP_IP = '161.6.10.102'
+TCP_PORT = 5822
+BUFFER_SIZE = 128
+MESSAGE = ""
+
+print "Welcome to my python network client!"
+
+while(MESSAGE != "exit"):
+	MESSAGE = raw_input("Enter some text: ")
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send(MESSAGE)
+	data = s.recv(BUFFER_SIZE)
+	s.close()
+	print "reply: ", data
+
+print "Exiting client program!"

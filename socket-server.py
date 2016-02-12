e64154b82d60103741c3b01e09d88642c674598f
goahnary <goahnary@goahnary.me>
2016-02-12 14:00:12 -0600

    Modified server script to accept inputs multiple times

--- socket-server.py
+++ socket-server.py
@@ -3,19 +3,21 @@
 import socket
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5823
-BUFFER_SIZE = 20
+TCP_PORT = 5822
+BUFFER_SIZE = 128
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', TCP_PORT))
-s.listen(1)
 
-conn, addr = s.accept()
-print 'connection address: ', addr
-while 1:
+while True:
+	s.listen(1)
+	conn, addr = s.accept()
+	print 'connection address: ', addr
 	data = conn.recv(BUFFER_SIZE)
 	if not data: break
+	data = data.upper()
 	print "received data:", data
 	conn.send(data) #send the data back
+
 conn.close()
 

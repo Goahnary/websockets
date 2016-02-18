cab4b0139a5d43e182c6444c78beb651a42dcbd2
Noah Gary <goahnary@gmail.com>
2016-02-18 12:02:03 -0600

    updated instructions and added comments to code

--- socket-server.py
+++ socket-server.py
@@ -2,23 +2,23 @@
 
 import socket
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5822
-BUFFER_SIZE = 128
+TCP_IP = '127.0.0.1'    # address of the server you would like to connect to
+TCP_PORT = 5829         # port you will connect on. THIS SHOULD MATCH THE SERVER FILE
+BUFFER_SIZE = 128       # small buffer size for text
 data = ""
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', TCP_PORT))
 
-while data != "EXIT":
-	s.listen(1)
-	conn, addr = s.accept()
-	print 'connection address: ', addr
-	data = conn.recv(BUFFER_SIZE)
-	if not data: break
-	data = data.upper()
+while data != "EXIT":				# loops unless it gets the exit command
+	s.listen(1)				# listens for response
+	conn, addr = s.accept()			# sets these two variables to socket accept object
+	print 'connection address: ', addr	# prints who is connecting
+	data = conn.recv(BUFFER_SIZE)		# grabs sent information from the s.accept object
+	if not data: break			# If there is nothing sent (sendiing null) break the loop
+	data = data.upper()			# capitalizes the data sent
 	print "received data:", data
-	conn.send(data) #send the data back
+	conn.send(data) 			# send the data back	
 
-conn.close()
+conn.close()					# closes connection
 
